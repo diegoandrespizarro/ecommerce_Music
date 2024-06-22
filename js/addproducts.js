020166b52b3b7e0f525e24b160625662c531ea43
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-06-22 16:16:47 -0300

    Update crud addproducts.js and style.css

--- js/addproducts.js
+++ js/addproducts.js
@@ -49,3 +49,57 @@ submitForm.addEventListener(`submit`, async (event) => {
     }
 });
 
+//MOSTRAR PRODUCTOS EN LA TABLA
+const productList = document.getElementById('product-list');
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        // Hacer una petición para obtener todos los productos
+        const response = await fetch(urlProducts);
+        const products = await response.json();
+
+        // Recorrer cada producto y renderizarlo en el DOM
+        products.forEach(product => {
+            const productCard = `
+                <tr>
+                    <td>${product.id}</td>
+                    <td>${product.title}</td>
+                    <td>${product.categoria}</td>
+                    <td>${product.precio}</td>
+                    <td><img src="${product.imageUrl}" alt="${product.title}" width="50"></td>
+                    <td>${product.descripcion}</td>
+                    <td>
+                        <button class="btn btn-danger delete-product" data-id="${product.id}">Eliminar</button>
+                        <button class="btn btn-success modificar-product" data-id="${product.id}">Editar</button>
+                    </td>
+                </tr>
+            `;
+            productList.innerHTML += productCard;
+        });
+
+    } catch (error) {
+        console.error("Error al obtener los productos", error);
+    }
+});
+//ELIMINAR PRODUCTOS DE LA TABLA
+document.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('delete-product')) {
+        const productId = event.target.getAttribute('data-id');
+        try {
+            const response = await fetch(`${urlProducts}/${productId}`, {
+                method: 'DELETE'
+            });
+
+            if (response.ok) {
+                alert("Producto eliminado exitosamente");
+                window.location.href = '/index.html';
+            } else {
+                alert("Error al eliminar el producto", response.statusText);
+            }
+        } catch (error) {
+            alert("Error al eliminar el producto", error);
+        }
+    }
+});
+
+
+
