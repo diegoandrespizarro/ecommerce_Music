7c43db4d791b2049cbb7e3bc7a92950ba413edea
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-06-30 16:54:41 -0300

    Update render API products in index.html

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 var nextBtn = document.querySelector('.next'),
     prevBtn = document.querySelector('.prev'),
     carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
+    list = document.querySelector('.list'),
     item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
+    runningTime = document.querySelector('.carousel .timeRunning')
 
-let timeRunning = 3000 
+let timeRunning = 3000
 let timeAutoNext = 7000
 
-nextBtn.onclick = function(){
+nextBtn.onclick = function () {
     showSlider('next')
 }
 
-prevBtn.onclick = function(){
+prevBtn.onclick = function () {
     showSlider('prev')
 }
 
-let runTimeOut 
+let runTimeOut
 
 let runNextAuto = setTimeout(() => {
     nextBtn.click()
@@ -26,24 +26,24 @@ let runNextAuto = setTimeout(() => {
 function resetTimeAnimation() {
     runningTime.style.animation = 'none'
     runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
+    runningTime.style.animation = null
     runningTime.style.animation = 'runningTime 7s linear 1 forwards'
 }
 
 
 function showSlider(type) {
     let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
-    if(type === 'next'){
+    if (type === 'next') {
         list.appendChild(sliderItemsDom[0])
         carousel.classList.add('next')
-    } else{
+    } else {
         list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
         carousel.classList.add('prev')
     }
 
     clearTimeout(runTimeOut)
 
-    runTimeOut = setTimeout( () => {
+    runTimeOut = setTimeout(() => {
         carousel.classList.remove('next')
         carousel.classList.remove('prev')
     }, timeRunning)
@@ -64,27 +64,27 @@ resetTimeAnimation()
 
 
 
-//RENDERIZO EN EL HTML LOS PRODUCTOS DE LA API
+//RENDERIZAMOS EN EL INDEX LOS PRODUCTOS DE LA API
 
 document.addEventListener('DOMContentLoaded', async () => {
     const productList = document.getElementById('product-list');
     const urlProducts = `https://6668e555f53957909ff96e69.mockapi.io/api/Products`;
-    
+
     try {
         // Hacer una petición para obtener todos los productos
         const response = await fetch(urlProducts);
         const products = await response.json();
-        
+
         // Recorrer cada producto y renderizarlo en el DOM
         products.forEach(product => {
-            // Convertir precio y stock a números
+            // CONVERTIMOS LOS VALORES DE STOCK Y PRECIO A NUMEROS USANDO PARSEFLOAT Y PARSEINT
             const precioNumber = parseFloat(product.precio);
             const stockNumber = parseInt(product.stock);
 
             if (isNaN(precioNumber) || isNaN(stockNumber)) {
-                console.error("Error: Precio o Stock no son números válidos", product);
                 return;
             }
+            // TRUNCAMOS LA DESCRIPCION Y EL TITULO DE LOS PRODUCTOS PARA QUE CUANDO EXCEDAN LOS 20 CARACTERES SE MUESTRE UN PUNTO SUSPENSIVO
             const truncatedDescription = product.descripcion.length > 20 ? `${product.descripcion.substring(0, 20)}...` : product.descripcion;
             const truncateTitle = product.title.length > 20 ? `${product.title.substring(0, 15)}...` : product.title;
 
@@ -102,15 +102,16 @@ document.addEventListener('DOMContentLoaded', async () => {
                     </div>
                 </div>
             `;
-
             productList.innerHTML += productCard;
         });
-        
     } catch (error) {
-        console.error("Error al obtener los productos", error);
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: (error),
+        });
     }
 });
-
 // FINAL DE RENDERIZADO DE PRODUCTOS DE LA API
 
 
